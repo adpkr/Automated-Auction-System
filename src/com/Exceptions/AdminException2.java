799fa91252e6a87036a809906d1b46077adffef2
adpkr <adp07070707@gmail.com>
2022-12-17 21:20:15 +0530

    4day

---
+++ src/com/Exceptions/AdminException2.java
@@ -0,0 +1,14 @@
+package com.Exceptions;
+
+public class AdminException2 extends Exception{
+	
+    public AdminException2() {
+		// TODO Auto-generated constructor stub
+	}
+    
+    public AdminException2(String msg) {
+    	super(msg);
+    }
+
+}
+                                                                            
